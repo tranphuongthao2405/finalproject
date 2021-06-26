7ff3d27f3c1033ab7d11f215c09038ff71d99b9a
tranphuongthao2405 <tranphuongthao24051999@gmail.com>
2021-06-27 00:10:18 +0700

    fix spell check

--- code/client/src/components/views/Admin/UserEdit.js
+++ code/client/src/components/views/Admin/UserEdit.js
@@ -161,7 +161,7 @@ function UserEdit(props) {
 
             <div className="form-row justify-content-center">
               <div className="form-group col-md-5">
-                <label htmlFor="role">Giới tính:</label>
+                <label htmlFor="role">Vai trò:</label>
                 <select
                   name="role"
                   className="form-control"
