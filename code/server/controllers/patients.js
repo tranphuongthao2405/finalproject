ad9b50e540633f75446d708f1c035081ec927173
tranphuongthao2405 <tranphuongthao24051999@gmail.com>
2021-07-11 16:36:34 +0700

    backup

--- code/server/controllers/patients.js
+++ code/server/controllers/patients.js
@@ -3,7 +3,7 @@ const { Patient } = require("../models/Patient");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, process.cwd() + "\\uploads\\");
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}_${file.originalname}`);
